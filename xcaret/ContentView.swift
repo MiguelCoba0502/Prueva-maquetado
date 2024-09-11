b4713623e08cd8a2675bebe8de0f63a96dc70c10
MiguelCoba0502 <miguel_8090@hotmail.com>
2024-09-10 20:58:33 -0600

    Initial Commit

---
+++ xcaret/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcaret
+//
+//  Created by Eduardo Coba on 10/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcaret/xcaretApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcaretApp.swift
+//  xcaret
+//
+//  Created by Eduardo Coba on 10/09/24.
+//
+
+import SwiftUI
+
+@main
+struct xcaretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
